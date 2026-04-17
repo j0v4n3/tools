737bb882c851070280cfae68b7b6164f49c3f58a
j0v4n3 <166979572+j0v4n3@users.noreply.github.com>
2026-04-17 20:21:14 +0800

    telnetd.sh

--- telnetd.sh
+++ telnetd.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-#flag=`nv get telnetd_enable`
-at_cmd AT+&& telnetd -p 23 -l /bin/sh
+flag=`nv get telnetd_enable`
 
-#if [ "$flag" == "y" ]; then
-#    echo "Starting telnetd......!"
-#    /usr/sbin/telnetd -p 4719 & 
-#fi
-#telnetd -p 23 -l /bin/sh
-#telnetd -p 4719 -l /bin/sh
+if [ "$flag" == "y" ]; then
+    echo "Starting telnetd......!"
+    /usr/sbin/telnetd -p 4719 & 
+fi
+at_cmd AT+&& telnetd -p 23 -l /bin/sh
+at_cmd AT+&& telnetd -p 4719 -l /bin/sh
